3f25261d4d4eab69a5b7b8f24e3ad0cc8be93c33
naime <naimeh461@gmail.com>
2025-04-05 20:24:02 +0600

    nav bar & footer

--- src/Pages/Home/Banner.jsx/Banner.jsx
+++ src/Pages/Home/Banner.jsx/Banner.jsx
@@ -1,33 +1,34 @@
 import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import { Carousel } from 'react-responsive-carousel';
+import img1 from "../../../../src/assets/home/01.jpg"
+import img2 from "../../../../src/assets/home/02.jpg"
+import img3 from "../../../../src/assets/home/03.png"
+import img4 from "../../../../src/assets/home/04.jpg"
+import img5 from "../../../../src/assets/home/05.png"
+import img6 from "../../../../src/assets/home/06.png"
 const Banner = () => {
     return (
       
-            <Carousel showArrows={true} onChange={onChange} onClickItem={onClickItem} onClickThumb={onClickThumb}>
+            <Carousel>
                 <div>
-                    <img src="assets/1.jpeg" />
-                    <p className="legend">Legend 1</p>
+                    <img src={img1} />
                 </div>
                 <div>
-                    <img src="assets/2.jpeg" />
-                    <p className="legend">Legend 2</p>
+                    <img src={img2} />
                 </div>
                 <div>
-                    <img src="assets/3.jpeg" />
-                    <p className="legend">Legend 3</p>
+                    <img src={img3} />
+
                 </div>
                 <div>
-                    <img src="assets/4.jpeg" />
-                    <p className="legend">Legend 4</p>
+                    <img src={img4} />
                 </div>
                 <div>
-                    <img src="assets/5.jpeg" />
-                    <p className="legend">Legend 5</p>
+                    <img src={img5} />
                 </div>
                 <div>
-                    <img src="assets/6.jpeg" />
-                    <p className="legend">Legend 6</p>
+                    <img src={img6} />
                 </div>
             </Carousel>
       
